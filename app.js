2dce1c3df20b7270d861c419b5777129ee8bf3dc
bashCandicoot <ste7@protonmail.com>
2018-09-25 11:54:38 +0100

    check number is a number, binary search locally first for speed

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 require('dotenv').config({ path: '.env' });
+const firstThousandPrimes = require('./firstThousandPrimes');
 
 const express = require('express');
 
 const app = express();
 
+function nearestPrime(number) {
+  binarySearchLocalArray(number);
+  // console.log(firstThousandPrimes);
+  return number;
+}
+
+function binarySearchLocalArray(number) {
+
+}
+
 app.get('/nearest-prime/:number', (req, res) => {
-  console.log(req.params.number);
+  const { number } = req.params;
+
+  if (isNaN(number)) return res.send({ message: `${number} is not a valid number` });
+
+  else if (number <= 1) return res.send({ nearestPrime: 1 });
+
+  res.send({ nearestPrime: nearestPrime(number) });
 });
 
 app.set('port', process.env.PORT);
