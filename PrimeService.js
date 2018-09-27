7764dcfd93197c26c99c74f7de701dc3316b3083
bashCandicoot <ste7@protonmail.com>
2018-09-27 21:53:27 +0100

    move logic into PrimeService

---
+++ PrimeService.js
@@ -0,0 +1,114 @@
+const _ = require('lodash');
+const fs = require('fs');
+
+class PrimeService {
+  constructor(number) {
+    this.number = number;
+    this.primes = [];
+  }
+
+  async readLocalPrimes() {
+    const data = await new Promise(((resolve) => {
+      resolve(fs.readFileSync('./localPrimes.csv', { encoding: 'utf8' }));
+    }));
+    this.primes = data.split(',').map(Number);
+  }
+
+  static writeLocalPrimes(sievedPrimes) {
+    fs.writeFile('./localPrimes.csv', sievedPrimes, 'utf8', (err) => {
+      if (err) {
+        console.error('Didn\'t write primes to .csv file');
+        throw new Error(err);
+      }
+    });
+  }
+
+  binarySearchLocalPrimes() {
+    const biggestLocalPrime = this.primes[this.primes.length - 1] || -1;
+    if (this.number > biggestLocalPrime) return -1;
+
+    let start = 0;
+    let end = this.primes.length - 1;
+    let middle = Math.floor((start + end) / 2);
+
+    while (this.primes[middle] !== this.number && start < end) {
+      if (this.number < this.primes[middle]) end = middle - 1;
+      else { start = middle + 1; }
+      middle = Math.floor((start + end) / 2);
+    }
+
+    const higherPrime = this.primes[middle + 1];
+    const lowerPrime = this.primes[middle];
+
+    if (!higherPrime) return [lowerPrime];
+
+    const higherDiff = Math.abs(this.number - higherPrime);
+    const lowerDiff = Math.abs(this.number - lowerPrime);
+    // check which prime our this.number is closest to
+    if (lowerDiff === higherDiff) return [lowerPrime, higherPrime];
+
+    return lowerDiff < higherDiff ? [lowerPrime] : [higherPrime];
+  }
+
+  static isPrime(number) {
+    const sqrt = Math.sqrt(number);
+    for (let i = 2; i <= sqrt; i++) {
+      if (number % i === 0) return false;
+    }
+
+    return true;
+  }
+
+  findNearestHigherPrime(lowerDiff) {
+    for (let i = 1; i <= lowerDiff; i++) {
+      if (PrimeService.isPrime(this.number + i)) {
+        return this.number + i;
+      }
+    }
+
+    return -1;
+  }
+
+  findNearestLowerPrime() {
+    const sqrt = Math.sqrt(this.number);
+
+    const sieve = {};
+    for (let i = 2; i <= this.number; i++) {
+      sieve[i] = true;
+    }
+
+    for (let i = 2; i < sqrt; i++) {
+      if (sieve[i]) {
+        for (let j = i ** 2; j <= this.number; j += i) {
+          delete sieve[j];
+        }
+      }
+    }
+
+    const sievedPrimes = [...Object.keys(sieve)];
+    PrimeService.writeLocalPrimes(sievedPrimes.join(','));
+    const biggestSievedPrime = sievedPrimes[sievedPrimes.length - 1];
+    return Number(biggestSievedPrime);
+  }
+
+  findNearestPrime() {
+    if (isNaN(this.number)) return [-1];
+    else if (this.number <= 3) return [2];
+
+    const localPrime = this.binarySearchLocalPrimes();
+    if (localPrime !== -1) return localPrime;
+
+    const lowerPrime = this.findNearestLowerPrime();
+    const lowerDiff = Math.abs(this.number - lowerPrime);
+
+    const higherPrime = this.findNearestHigherPrime(lowerDiff);
+    const higherDiff = Math.abs(this.number - higherPrime);
+
+    if (lowerDiff === higherDiff) return [lowerPrime, higherPrime];
+    if (higherPrime !== -1) return [higherPrime];
+    return [lowerPrime];
+  }
+}
+
+module.exports = PrimeService;
+
--- app.js
+++ app.js
@@ -1,114 +1,18 @@
 require('dotenv').config({ path: '.env' });
-const _ = require('lodash');
 const express = require('express');
 const fs = require('fs');
 
+const PrimeService = require('./PrimeService');
+
 const app = express();
 
-function readLocalPrimes() {
-  return new Promise(((resolve) => {
-    resolve(fs.readFileSync('./localPrimes.csv', { encoding: 'utf8' }));
-  }));
-}
-
-function writeLocalPrimes(primes) {
-  fs.writeFile('./localPrimes.csv', primes, 'utf8', (err) => {
-    if (err) console.error('Didn\'t write primes to .csv file');
-  });
-}
-
-function binarySearchLocalPrimes(primes, number) {
-  const biggestLocalPrime = primes[primes.length - 1];
-  if (number > biggestLocalPrime) return -1;
-
-  let start = 0;
-  let end = primes.length - 1;
-  let middle = Math.floor((start + end) / 2);
-
-  while (primes[middle] !== number && start < end) {
-    if (number < primes[middle]) end = middle - 1;
-    else { start = middle + 1; }
-    middle = Math.floor((start + end) / 2);
-  }
-
-  const higherPrime = primes[middle + 1];
-  const lowerPrime = primes[middle];
-
-  if (!higherPrime) return [lowerPrime];
-
-  const higherDiff = Math.abs(number - higherPrime);
-  const lowerDiff = Math.abs(number - lowerPrime);
-
-  // check which prime our number is closest to
-  if (lowerDiff === higherDiff) return [lowerPrime, higherPrime];
-  return lowerDiff < higherDiff ? [lowerPrime] : [higherPrime];
-}
-
-function isPrime(number) {
-  const sqrt = Math.sqrt(number);
-  for (let i = 2; i <= sqrt; i++) {
-    if (number % i === 0) return false;
-  }
-  return true;
-}
-
-function findNearestHigherPrime(number, lowerPrimeDiff) {
-  for (let i = 0; i <= lowerPrimeDiff; i++) {
-    if (isPrime(number)) return [number];
-  }
-
-  return -1;
-}
-
-function findNearestLowerPrime(primes, number) {
-  const sqrt = Math.sqrt(number);
-
-  const sieve = {};
-  for (let i = 2; i <= number; i++) {
-    sieve[i] = true;
-  }
-
-  for (let i = 2; i < sqrt; i++) {
-    if (sieve[i]) {
-      for (let j = i ** 2; j <= number; j += i) {
-        delete sieve[j];
-      }
-    }
-  }
-
-  const sievedPrimes = [...Object.keys(sieve)];
-  writeLocalPrimes(sievedPrimes.join(','));
-
-  const biggestSievedPrime = sievedPrimes[sievedPrimes.length - 1];
-  return [Number(biggestSievedPrime)];
-}
-
-function findNearestPrime(localPrimes, number) {
-  const localPrime = binarySearchLocalPrimes(localPrimes, number);
-  if (localPrime !== -1) return localPrime;
-
-  const lowerPrime = findNearestLowerPrime(localPrimes, number);
-  const lowerPrimeDiff = number - lowerPrime;
-
-  const higherPrime = findNearestHigherPrime(number + 1, lowerPrimeDiff);
-  const higherPrimeDiff = higherPrime - number;
-
-  if (lowerPrimeDiff === higherPrimeDiff) return [lowerPrime, higherPrime];
-  if (higherPrime !== -1) return higherPrime;
-  return lowerPrime;
-}
-
 app.get('/nearest-prime/:number', async (req, res) => {
-  const data = await readLocalPrimes();
-  const localPrimes = data.split(',').map(Number);
-
   const number = Number(req.params.number);
 
-  if (isNaN(number)) return res.send({ nearestPrime: [-1] });
+  const prime = new PrimeService(number);
+  await prime.readLocalPrimes();
 
-  else if (number <= 2) return res.send({ nearestPrime: [2] });
-
-  return res.send({ nearestPrime: findNearestPrime(localPrimes, number) });
+  return res.send({ nearestPrime: prime.findNearestPrime() });
 });
 
 app.set('port', 8081);
